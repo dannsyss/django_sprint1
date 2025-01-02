53a75a0cccbaacb13760be904c23a59bafad1b36
dannsyss <danilasysolatin.ed@gmail.com>
2025-01-02 22:51:15 +0600

    1

--- blogicum/blogicum/settings.py
+++ blogicum/blogicum/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-qiamu1r3!5uibkx1y1=++f2dbx4le##1d=hs*1zls4enc89@nf
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1"]
 
 
 # Application definition
